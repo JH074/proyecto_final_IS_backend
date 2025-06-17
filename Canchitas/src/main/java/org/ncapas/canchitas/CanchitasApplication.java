1809776ba2cbed84fd043391f71a944fd42abc1d
DiegoDomin <00081022@uca.edu.sv>
2025-06-16 22:24:43 -0600

    feat: aplication.java arreglado

---
+++ Canchitas/src/main/java/org/ncapas/canchitas/CanchitasApplication.java
@@ -0,0 +1,13 @@
+package org.ncapas.canchitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanchitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanchitasApplication.class, args);
+    }
+
+}
