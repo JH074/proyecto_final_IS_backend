a8cd73e476a7eeb306a6bd2a2a20616fd7a8d0eb
elKelvinRivas <00083522@uca.edu.sv>
2025-11-15 17:17:25 -0600

    Add: Controller :memo:

---
+++ Canchitas/src/main/java/org/ncapas/canchitas/Controllers/CalificacionController.java
@@ -0,0 +1,26 @@
+package org.ncapas.canchitas.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.ncapas.canchitas.Service.CalificacionService;
+import org.ncapas.canchitas.DTOs.request.CalificacionRequestDTO;
+
+@RestController
+@RequestMapping("/api/calificaciones")
+@RequiredArgsConstructor
+public class CalificacionController {
+
+    private final CalificacionService service;
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody CalificacionRequest request) {
+        return ResponseEntity.ok(service.crearCalificacion(request));
+    }
+
+    @GetMapping("/promedio/{idCancha}")
+    public ResponseEntity<?> obtenerPromedio(@PathVariable Integer idCancha) {
+        return ResponseEntity.ok(service.obtenerPromedioDeCancha(idCancha));
+    }
+}
